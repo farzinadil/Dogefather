336a9746cfb40ea09424ec4b6f6bd89ce3fb89c9
farzinadil <farzinadil@gmail.com>
2021-02-09 00:49:33 -0800

    added readme and added comments to code

--- TweetBot.py
+++ TweetBot.py
@@ -11,19 +11,24 @@ from imageai.Detection import ObjectDetection
 from difflib import SequenceMatcher
 
 
-def sid():
+# ger twilio account sid
+def sid(): 
     return os.getenv("TWILIO_ACCOUNT_SID")
 
+#get twilio auth token
 def twilioAuth():
     return os.getenv("TWILIO_AUTH_TOKEN")
 
+#get twitter bearer token
 def auth():
     return os.getenv("BEARER_TOKEN")
 
+# funtion that returns witter timeline url 
 def create_url():
     user_id = 44196397 # elon: 44196397 # test account: 1358539990670536705
     return "https://api.twitter.com/2/users/{}/tweets".format(user_id)
 
+#returns tweet parameters for twitter api 
 def get_params():
     # Tweet fields are adjustable.
     # Options include:
@@ -34,11 +39,12 @@ def get_params():
     # source, text, and withheld
     return {"tweet.fields": "created_at,attachments"}
 
+#authenticate bearer token 
 def create_headers(bearer_token):
     headers = {"Authorization": "Bearer {}".format(bearer_token)}
     return headers
 
-
+# connect to twitter api and return json
 def connect_to_endpoint(url, headers, params):
     response = requests.request("GET", url, headers=headers, params=params)
     print(response.status_code)
@@ -50,6 +56,7 @@ def connect_to_endpoint(url, headers, params):
         )
     return response.json()
 
+# function that return similarity index of 2 strings
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
@@ -76,46 +83,45 @@ def main():
     detector.setModelPath(os.path.join(current_directory, "yolo.h5"))
     detector.loadModel()
 
+    #constant loop that runs every 2 minutes
     while True:
-        json_response = connect_to_endpoint(url, headers, params)
+        json_response = connect_to_endpoint(url, headers, params) #get latest tweets
         #print(json.dumps(json_response, indent=4, sort_keys=True))
         tweets = json_response['data']
         #print(tweets)
         print("Search: ")
         for tweet in tweets:
-            tweetDate = datetime.strptime(tweet['created_at'][:-1], '%Y-%m-%dT%H:%M:%S.%f')
-            if tweetDate > lastDate:
-                tweetContent = tweet['text'].lower()
-                splitTweetContent = tweetContent.split()
+            tweetDate = datetime.strptime(tweet['created_at'][:-1], '%Y-%m-%dT%H:%M:%S.%f') #get the time the tweet was posted
+            if tweetDate > lastDate: # check if tweet has already been analyzed based on time of last tweet analyzed
+                tweetContent = tweet['text'].lower() #format all letters to lowercase 
+                splitTweetContent = tweetContent.split() #split tweet into words 
 
 
-                with open ('cryptos.json') as cryptoJsonFile:
-                    cryptoData = json.load(cryptoJsonFile)
-                    for crypto in cryptoData['cryptos']:
-                        for word in splitTweetContent:
-                            if similar(word, crypto['symbol'].lower() ) > 0.75 or similar(word, crypto['name'].lower()) > 0.65 :
-                                cryptoMessage = "crypto found: " + crypto['name'] + " in Elon's tweet: " + tweet['text']
+                with open ('cryptos.json') as cryptoJsonFile: 
+                    cryptoData = json.load(cryptoJsonFile) #get cryptodata from cryptos.json file
+                    for crypto in cryptoData['cryptos']: #iterate through each cryto 
+                        for word in splitTweetContent: #iterate through each word in the tweet
+                            if similar(word, crypto['symbol'].lower() ) > 0.75 or similar(word, crypto['name'].lower()) > 0.65 : #check if word is similar to crypto symbol or name
+                                cryptoMessage = "crypto found: " + crypto['name'] + " in Elon's tweet: " + tweet['text'] #generate crypto found message
                                 print(cryptoMessage)
-                                '''
-
+                               #send SMS with Twilio
                                 for number in numbers_to_message:
                                     client.messages.create(
                                         body=cryptoMessage,
                                         from_='+00000000',
                                         to=number
                                     )
-                                    '''
 
                                 print("\n")
 
                 with open('stocks.json') as stocksJsonFile:
-                    stockData = json.load(stocksJsonFile)
-                    for stock in stockData['popular_stocks']:
-                        for word in splitTweetContent:
-                            if (similar(word, stock['name'].lower() ) > 0.75 ): #or similar(word, stock['tinker'].lower() ) > 0.9
-                                stockMessage = "stock found: " + stock['name'] + " in Elon's tweet: " + tweet['text']
+                    stockData = json.load(stocksJsonFile) #get stockData from stocks.json file
+                    for stock in stockData['popular_stocks']: #iterate through each stock
+                        for word in splitTweetContent: #iterate through each word in the tweet
+                            if (similar(word, stock['name'].lower() ) > 0.75 ): #check if word is similar to stock name
+                                stockMessage = "stock found: " + stock['name'] + " in Elon's tweet: " + tweet['text'] #generate stock found message
                                 print(stockMessage)
-                                '''
+                                #send SMS with Twilio
                                 for number in numbers_to_message:
                                     client.messages.create(
                                         body=stockMessage,
@@ -123,32 +129,32 @@ def main():
                                         to=number
                                     )
                                 print("\n")
-                                '''
-
+                               
+                # check tweets for images
                 try:
-                    if "attachments" in tweet:
-                        requestURL = "https://api.twitter.com/2/tweets?ids=" + tweet['id'] +"&expansions=attachments.media_keys&media.fields=duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width"
+                    if "attachments" in tweet: # if tweet potentially contains images
+                        requestURL = "https://api.twitter.com/2/tweets?ids=" + tweet['id'] +"&expansions=attachments.media_keys&media.fields=duration_ms,height,media_key,preview_image_url,public_metrics,type,url,width" #send request to get tweet by tweet ID
                         requestTweet = requests.get(requestURL, headers={"Authorization": "Bearer {}".format(bearer_token)})
-                        for media in requestTweet.json()['includes']['media']:
+                        for media in requestTweet.json()['includes']['media']: #iterate through media array of tweet
                             imageURL = media['url']
-                            urllib.request.urlretrieve(imageURL,"tweeted-image.jpg")
+                            urllib.request.urlretrieve(imageURL,"tweeted-image.jpg") #retrieve and download image from media 
                             time.sleep(5)
+                            #use imageAI to detect objects in image
                             detections = detector.detectObjectsFromImage(
                                 input_image=os.path.join(current_directory, "tweeted-image.jpg"),
                                 output_image_path=os.path.join(current_directory, "labeled-tweeted-image.jpg"))
-                            for eachObject in detections:
-                                if eachObject["name"] == "dog":
+                            for eachObject in detections: #iterate through each object in image
+                                if eachObject["name"] == "dog": #check if object is dog
                                     dogeMessage = "found dog in image: " + imageURL
                                     print(dogeMessage)
-                                    '''
-
+                                    #send SMS with Twilio
                                     for number in numbers_to_message:
                                         client.messages.create(
                                             body=dogeMessage,
                                             from_='+000000000',
                                             to=number
                                         )
-                                        '''
+                                        
                 except:
                     print("error")
 
---
+++ readme.md
@@ -0,0 +1,24 @@
+# DogeCEO
+
+## About
+This is a program that checks for new tweets from a Twitter user (Elon Musk by default) and checks if the tweets contain the name of popular stocks or cryptocurrencies. The program checks for tweets every 2 minutes. If the tweet contains text, it will scan the text for potential names of companies with commonly traded stocks or popular cryptocurrencies. If the tweet contains an image or multiple images, the program will use ImageAI to locate images of Dogecoin or images of dogs. If the program finds images of dogecoin or text containing stocks or cryptocurrencies, it will send an SMS to the configured phone number using the Twilio API.
+
+## Installatin
+
+1. <code>pip install -U python-dotenv</code>
+2. <code>pip install pip install twilio</code>
+3. <code>pip install numpy</code>
+4. <code>pip install tensorflow</code>
+5. <code>pip install keras</code>
+6. <code>pip install opencv-python</code>
+7. <code>pip install imageai --upgrade</code>
+8. Create a .env file add Twitter API consumer key, consumer secret key, and bearer token as shown in .env.example
+9. Add the Twilio account sid and Twilio auth token to .env file as shown in the .env.example file
+10. Replace line 67 with an array of phone numbers to SMS when stocks or cryptos are found in tweets.
+```python
+numbers_to_message = ['+000000000']
+```
+11. Replace line 110, 127, amd 151 with Twilio number to send messages from. 
+```python
+from_='+000000000'
+```
